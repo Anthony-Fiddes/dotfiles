da45addbe40b934a8b9d856e399b55d31cc0e449
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2022-02-16 10:37:04 -0500

    Properly configure LspInstaller, add prose linters. Introduce afiddes namespace

--- nvim/init.lua
+++ nvim/init.lua
@@ -140,19 +140,16 @@ local function load_plugins()
 				local null_ls = require("null-ls")
 				local sources = {
 					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.diagnostics.write_good.with({
+						extra_filetypes = { "pandoc" },
+					}),
+					null_ls.builtins.diagnostics.proselint.with({
+						extra_filetypes = { "pandoc" },
+					}),
 				}
 				null_ls.setup({
 					sources = sources,
-					on_attach = function(client)
-						if client.resolved_capabilities.document_formatting then
-							vim.cmd([[ 
-							augroup LspFormatting 
-								autocmd! * <buffer>
-								autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync() 
-							augroup END 
-							]])
-						end
-					end,
+					on_attach = require("afiddes/lsp-config").on_attach,
 				})
 			end,
 			requires = { "nvim-lua/plenary.nvim" },
@@ -358,66 +355,17 @@ execute "digraphs yS " . 0x02b8
 execute "digraphs zS " . 0x1dbb
 ]])
 
---- LSP Keybindings ---
-local nvim_lsp = require("lspconfig")
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-	local function buf_set_keymap(...)
-		vim.api.nvim_buf_set_keymap(bufnr, ...)
-	end
-	local function buf_set_option(...)
-		vim.api.nvim_buf_set_option(bufnr, ...)
-	end
-
-	-- Enable completion triggered by <c-x><c-o>
-	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
-	-- Mappings.
-	local opts = { noremap = true, silent = true }
-
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	buf_set_keymap("n", "<Leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "<Leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-	buf_set_keymap("n", "<Leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-	buf_set_keymap("n", "<Leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-	buf_set_keymap("n", "<Leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	buf_set_keymap("n", "<Leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	buf_set_keymap("n", "<Leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-	buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-	buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-	buf_set_keymap("n", "<Leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-	buf_set_keymap("n", "<Leader>fo", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-end
-
 local lsp_installer = require("nvim-lsp-installer")
 
 -- Register a handler that will be called for each installed server when it's
 -- ready (i.e. when installation is finished
 -- or if the server is already installed).
 lsp_installer.on_server_ready(function(server)
-	local opts = {}
+	local opts = {
+		on_attach = require("afiddes/lsp-config").on_attach,
+	}
 	-- This setup() function will take the provided server configuration and decorate it with the necessary properties
 	-- before passing it onwards to lspconfig.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
 end)
-
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
-local servers = { "gopls", "sumneko_lua" }
-for _, lsp in ipairs(servers) do
-	nvim_lsp[lsp].setup({
-		on_attach = on_attach,
-		flags = {
-			debounce_text_changes = 150,
-		},
-	})
-end
