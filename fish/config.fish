41dc8db537eeaabf0078cb0eafeb59b78385863f
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2022-02-28 23:38:55 -0500

    Add more abbreviations and z plugin

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,6 @@ alias icat="kitty +kitten icat"
 alias ipython=ipython3
 alias open=xdg-open
 alias python=python3
-alias vim=nvim
 alias vimm="command vim"
 
 # PATH Variables
@@ -25,13 +24,17 @@ if not set -q XDG_CONFIG_HOME
 end
 
 # Abbreviations
+abbr -a -g cat batcat
 abbr -a -g ga git add
 abbr -a -g gc git commit -v
 abbr -a -g gca git commit -v --amend
+abbr -a -g gd git diff
 abbr -a -g gp git push
 abbr -a -g gpl git pull
 abbr -a -g gs git status
 abbr -a -g sf source "$XDG_CONFIG_HOME/fish/config.fish"
+abbr -a -g v nvim
+abbr -a -g vim nvim
 
 # Vim Mode
 fish_vi_key_bindings
