87922792899fda7be9b4d5cc9ad7bf6a1e651e8c
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2022-06-06 13:19:38 -0700

    Use rclone to delete dependency on gdrive_crypt service
    gdrive_crypt still needs to be setup as an rclone endpoint!

--- systemd/user/budgeter_backup.sh
+++ systemd/user/budgeter_backup.sh
@@ -5,5 +5,5 @@ DBNAME=budget_backup.db
 set -e
 
 # In case there's some kind of error in my program, I'll use a temp file.
-~/go/bin/budgeter backup ~/.$DBNAME.tmp 
-mv ~/.$DBNAME.tmp ~/gdrive_crypt/Finances/Budgets/$DBNAME
+~/go/bin/budgeter backup "$HOME/.${DBNAME}.tmp"
+rclone move "$HOME/.$DBNAME.tmp" gdrive_crypt:Backups/ -v
