e343ec21108efeaa22ef071949782636b2a131f1
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-03-17 21:05:05 -0700

    Open nvim-tree on folders

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,6 +29,20 @@ local function load_plugins()
 			requires = { "kyazdani42/nvim-web-devicons" },
 			config = function()
 				require("nvim-tree").setup({})
+				local function open_nvim_tree(data)
+					-- buffer is a directory
+					local directory = vim.fn.isdirectory(data.file) == 1
+					if not directory then
+						return
+					end
+
+					-- change to the directory
+					vim.cmd.cd(data.file)
+					-- open the tree
+					require("nvim-tree.api").tree.open()
+				end
+
+				vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 			end,
 		})
 		use({
