1caadc47a74d0408c8642e8cb7eeaeddf0fdaf3a
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-07-04 17:28:10 -0700

    Add basic installation scripts. WIP

---
+++ bin/install.fish
@@ -0,0 +1,53 @@
+#!/usr/bin/fish
+
+# Requirements
+sudo apt install libfuse2 gh bat ripgrep
+if not set -q XDG_CONFIG_HOME
+  set -g XDG_CONFIG_HOME $HOME/.config  
+end
+
+# Get useful executables (just debian linux for now)
+mkdir -p $HOME/bin
+fish_add_path -U $HOME/bin
+
+if not type -q nvim
+  curl -o $HOME/bin/nvim -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage 
+  chmod u+x $HOME/bin/nvim
+end
+
+if not type -q brave-browser
+  sudo curl -fsslo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  sudo apt update
+  sudo apt install brave-browser
+end
+
+# configure fish
+rm -rf $XDG_CONFIG_HOME/fish
+ln -s $(pwd)/fish $XDG_CONFIG_HOME/fish
+
+# configure neovim
+rm -rf $XDG_CONFIG_HOME/nvim
+ln -s $(pwd)/nvim $XDG_CONFIG_HOME/nvim
+# run twice because i find that it hangs the first time. after the first install
+# it's usually pretty quick.
+timeout 15s nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+timeout 15s nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
+# configure kitty
+rm -rf $XDG_CONFIG_HOME/kitty
+ln -s $(pwd)/kitty $XDG_CONFIG_HOME/kitty
+if not fc-list | grep -q -i caskaydiacove
+  set cascadia "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.tar.xz"
+  mkdir -p $HOME/.fonts
+  curl -s -L $cascadia | tar xvfJ - --directory=$HOME/.fonts/
+  sudo fc-cache -fv
+end
+
+# configure gh/git
+if not gh auth status
+  gh auth login
+end
+
+rm -rf $HOME/.gitconfig
+cp ./.gitconfig $HOME/.gitconfig
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Get the basics
+sudo apt install git fish kitty tldr curl
+chsh -s $(which fish) $USER
+update-alternatives --config x-terminal-emulator
+
+# Keep going in fish
+./bin/install.fish
