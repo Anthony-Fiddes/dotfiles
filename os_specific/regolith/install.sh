a03007164aaefaff55f109718284d175d4a24cae
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-07-15 16:04:06 -0700

    Change hjkl to neio for moving windows as well

--- os_specific/regolith/install.sh
+++ os_specific/regolith/install.sh
@@ -11,3 +11,12 @@ cp ./82_rofication-ilia_replacement ~/.config/regolith2/i3/config.d/
 
 sudo apt remove regolith-i3-i3xrocks
 cp ./70_bar_replacement ~/.config/regolith2/i3/config.d/
+
+sudo apt remove regolith-i3-workspace-config
+cp ./40_workspace-config_replacement ~/.config/regolith2/i3/config.d/
+
+sudo apt remove regolith-i3-control-center-regolith
+cp ./60_config_keybindings_replacement ~/.config/regolith2/i3/config.d/
+
+i3-msg reload
+
