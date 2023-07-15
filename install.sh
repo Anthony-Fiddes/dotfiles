b43db06b19f1ab9ef5fc119464a3f11e65d4473e
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-07-14 17:27:01 -0700

    Add make as dep

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the basics
-sudo apt install git fish kitty tldr curl
+sudo apt install git fish kitty tldr curl make
 chsh -s $(which fish) $USER
 update-alternatives --config x-terminal-emulator
 
