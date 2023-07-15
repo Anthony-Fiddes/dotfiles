cef20e9a165a8c5c7c5a7515d2f17626556c40ef
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-07-14 20:45:46 -0700

    Make n e i o the basic layout.
    More customization needed. May just need to start from scratch with i3
    honestly.

--- os_specific/regolith/install.sh
+++ os_specific/regolith/install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-sudo apt remove regolith-i3-session
 mkdir -p ~/.config/regolith2/i3/config.d/
+sudo apt remove regolith-i3-session
 cp ./55_session_keybindings_replacement ~/.config/regolith2/i3/config.d/
+
+sudo apt remove regolith-i3-navigation
+cp ./30_navigation_replacement ~/.config/regolith2/i3/config.d/
+
+sudo apt remove regolith-i3-rofication-ilia
+cp ./82_rofication-ilia_replacement ~/.config/regolith2/i3/config.d/
+
+sudo apt remove regolith-i3-i3xrocks
+cp ./70_bar_replacement ~/.config/regolith2/i3/config.d/
