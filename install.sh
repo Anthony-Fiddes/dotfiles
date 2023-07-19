79a91864a45262b4f5df8ed0a1e04560ce21f8ca
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-07-18 20:47:13 -0700

    Install the correct fish and use correct font in kitty

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Get the basics
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt upgrade
 sudo apt install git fish kitty tldr curl make
 chsh -s $(which fish) $USER
 update-alternatives --config x-terminal-emulator
