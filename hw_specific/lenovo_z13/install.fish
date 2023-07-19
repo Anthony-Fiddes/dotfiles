7204dea8b598d5a236beb2c6cb3a39ed04c11869
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-07-18 23:02:42 -0700

    Add basic lenovo setup

---
+++ hw_specific/lenovo_z13/install.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/fish
+
+# Set up fingerprints
+sudo apt install fprintd libpam-fprintd
+sudo pam-auth-update
+
+
+
---
+++ hw_specific/lenovo_z13/resources.md
@@ -0,0 +1,1 @@
+[Some guy's awesome guide](https://blog.15cm.net/2022/08/21/my_arch_linux_setup_on_thinkpad_z13_gen_1/)
