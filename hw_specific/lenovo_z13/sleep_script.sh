24cec75373c85e47a839b0a945400e4ee4eb688d
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-07-30 20:08:19 -0700

    Turn off wifi and touchpad when the laptop goes to sleep
    Turn them on when it comes back.

--- hw_specific/lenovo_z13/sleep_script.sh
+++ hw_specific/lenovo_z13/sleep_script.sh
@@ -3,17 +3,16 @@
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
 
 case "$1" in
-	#code execution BEFORE sleeping/hibernating/suspending
+    #code execution BEFORE sleeping/hibernating/suspending
     pre)
+        rfkill toggle wlan
+        xinput set-prop "ELAN06A0:00 04F3:3231 Touchpad" "Device Enabled" 0
     ;;
 	#code execution AFTER resuming
     post)
-		# Fix wifi speeds
-		rfkill toggle wlan
-		rfkill toggle wlan
+        # Fix wifi speeds
+        rfkill toggle wlan
         # Toggle touchpad to make two finger right clicks more reliable
-        xinput set-prop "ELAN06A0:00 04F3:3231 Touchpad" "Device Enabled" 0
-        sleep 0.1
         xinput set-prop "ELAN06A0:00 04F3:3231 Touchpad" "Device Enabled" 1
     ;;
 esac
