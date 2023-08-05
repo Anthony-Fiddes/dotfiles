8cf433307f6b0452c327d34697a3245dd3b6faf3
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-08-05 12:58:36 -0700

    Backup pop os desktop keybindings

--- os_specific/pop_os/backup.fish
+++ os_specific/pop_os/backup.fish
@@ -1,2 +1,3 @@
 #!/usr/bin/fish
 cp $XDG_CONFIG_HOME/touchegg/touchegg.conf ./touchegg.conf 
+dconf dump '/org/gnome/desktop/wm/keybindings/' > keybindings.dconf
--- os_specific/pop_os/install.fish
+++ os_specific/pop_os/install.fish
@@ -1,2 +1,3 @@
 #!/usr/bin/fish
 cp ./touchegg.conf $XDG_CONFIG_HOME/touchegg/touchegg.conf
+dconf load '/org/gnome/desktop/wm/keybindings/' < keybindings.dconf
