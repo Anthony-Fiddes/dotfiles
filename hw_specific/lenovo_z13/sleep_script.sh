6ac07d92fb97a2fb1340e48beaec421663690616
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-08-12 21:44:32 -0700

    Add back wifi reset after sleep.
    Still seeing the issue with after only turning off wifi power
    management.

--- hw_specific/lenovo_z13/sleep_script.sh
+++ hw_specific/lenovo_z13/sleep_script.sh
@@ -3,15 +3,18 @@
 PATH=/sbin:/usr/sbin:/bin:/usr/bin
 
 case "$1" in
-    #code execution BEFORE sleeping/hibernating/suspending
-    pre)
-        xinput set-prop "ELAN06A0:00 04F3:3231 Touchpad" "Device Enabled" 0
-    ;;
+	#code execution BEFORE sleeping/hibernating/suspending
+	pre)
+		rfkill toggle wlan
+		xinput set-prop "ELAN06A0:00 04F3:3231 Touchpad" "Device Enabled" 0
+	;;
 	#code execution AFTER resuming
-    post)
-        # Toggle touchpad to make two finger right clicks more reliable
-        xinput set-prop "ELAN06A0:00 04F3:3231 Touchpad" "Device Enabled" 1
-    ;;
+	post)
+		# Fix wifi speeds
+		rfkill toggle wlan
+		# Toggle touchpad to make two finger right clicks more reliable
+		xinput set-prop "ELAN06A0:00 04F3:3231 Touchpad" "Device Enabled" 1
+	;;
 esac
 
 exit 0
