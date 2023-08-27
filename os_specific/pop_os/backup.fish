a46842cb530cad3be7838b7e11abc6daf3d4db38
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-08-27 14:11:11 -0700

    Add pop-shell settings

--- os_specific/pop_os/backup.fish
+++ os_specific/pop_os/backup.fish
@@ -1,3 +1,4 @@
 #!/usr/bin/fish
-cp $XDG_CONFIG_HOME/touchegg/touchegg.conf ./touchegg.conf 
-dconf dump '/org/gnome/desktop/wm/keybindings/' > keybindings.dconf
+cp $XDG_CONFIG_HOME/touchegg/touchegg.conf ./touchegg.conf
+dconf dump /org/gnome/desktop/wm/keybindings/ >keybindings.dconf
+dconf dump /org/gnome/shell/extensions/pop-shell/ >pop-shell.dconf
--- os_specific/pop_os/install.fish
+++ os_specific/pop_os/install.fish
@@ -1,3 +1,5 @@
 #!/usr/bin/fish
 cp ./touchegg.conf $XDG_CONFIG_HOME/touchegg/touchegg.conf
-dconf load '/org/gnome/desktop/wm/keybindings/' < keybindings.dconf
+dconf load /org/gnome/desktop/wm/keybindings/ <keybindings.dconf
+dconf load /org/gnome/shell/extensions/pop-shell/ <pop-shell.dconf
+
