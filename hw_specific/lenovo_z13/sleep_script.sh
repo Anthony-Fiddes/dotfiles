3495897762dde85b903c27a1b38ed97916390e79
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-09-17 14:19:52 -0700

    Remove modprobe hack as it's no longer necessary

--- hw_specific/lenovo_z13/sleep_script.sh
+++ hw_specific/lenovo_z13/sleep_script.sh
@@ -9,8 +9,6 @@ case "$1" in
 	;;
 	#code execution AFTER resuming
 	post)
-		# Fix wifi speeds by reloading module
-		sudo modprobe -r ath11k_pci && sudo modprobe ath11k_pci
 		# Toggle touchpad to make two finger right clicks more reliable
 		xinput set-prop "ELAN06A0:00 04F3:3231 Touchpad" "Device Enabled" 1
 	;;
