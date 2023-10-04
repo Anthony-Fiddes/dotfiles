0a5f8d2aaaa141c309e90363efaa902714a1c665
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-10-03 17:53:29 -0700

    Add updated git and upgrade nvim/

--- bin/install.fish
+++ bin/install.fish
@@ -1,13 +1,16 @@
 #!/usr/bin/fish
 
 # Requirements
-sudo apt install libfuse2 
+sudo apt install libfuse2
 if not set -q XDG_CONFIG_HOME
-  set -g XDG_CONFIG_HOME $HOME/.config  
+    set -g XDG_CONFIG_HOME $HOME/.config
 end
 
 # Install useful cli tools
-sudo apt install htop gh bat ripgrep fzf fd-find rclone 
+sudo apt install htop gh bat ripgrep fzf fd-find rclone
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt update
+sudo apt install git
 
 # Install useful gui tools
 sudo apt install dconf-editor
@@ -21,23 +24,23 @@ mkdir -p $HOME/bin
 fish_add_path -U $HOME/bin
 
 if not type -q nvim
-  curl -o $HOME/bin/nvim -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage 
-  chmod u+x $HOME/bin/nvim
+    curl -o $HOME/bin/nvim -L https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x $HOME/bin/nvim
 end
 
 if not type -q brave-browser
-  sudo curl -fsslo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-  sudo apt update
-  sudo apt install brave-browser
+    sudo curl -fsslo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    sudo apt update
+    sudo apt install brave-browser
 end
 
 # configure fish
 rm -rf $XDG_CONFIG_HOME/fish
 ln -s $(pwd)/fish $XDG_CONFIG_HOME/fish
 if not type -q fisher
-  curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
-  tide configure
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher update
+    tide configure
 end
 
 # configure neovim
@@ -51,16 +54,15 @@ sudo apt install python3.10-venv
 rm -rf $XDG_CONFIG_HOME/kitty
 ln -s $(pwd)/kitty $XDG_CONFIG_HOME/kitty
 if not fc-list | grep -q -i caskaydiacove
-  set cascadia "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.tar.xz"
-  mkdir -p $HOME/.fonts
-  curl -s -L $cascadia | tar xvfJ - --directory=$HOME/.fonts/
-  sudo fc-cache -fv
+    set cascadia "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.tar.xz"
+    mkdir -p $HOME/.fonts
+    curl -s -L $cascadia | tar xvfJ - --directory=$HOME/.fonts/
+    sudo fc-cache -fv
 end
 
 # configure gh/git
 if not gh auth status
-  rm -f $HOME/.gitconfig
-  cp ./.gitconfig $HOME/.gitconfig
-  gh auth login
+    rm -f $HOME/.gitconfig
+    cp ./.gitconfig $HOME/.gitconfig
+    gh auth login
 end
-
