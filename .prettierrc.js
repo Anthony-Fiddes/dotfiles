359c3f2eef13f38ef997a557ceb3433b69f00b79
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-11-29 09:28:40 -0800

    Remove prettierrc and update nvim

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	plugins: ["/usr/lib/node_modules/prettier-plugin-sql-cst/dist/index.js"],
-	overrides: [
-		{
-			files: ["*.sql"],
-			options: { parser: "bigquery" },
-		},
-	],
-};
