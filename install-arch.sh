b6469b5cbf635ae2ef6971f88e614653ae851e45
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-12-08 17:55:56 -0800

    Enable bluetooth

--- install-arch.sh
+++ install-arch.sh
@@ -4,6 +4,8 @@ yay -S fish kitty neovim fzf ripgrep github-cli go htop bat
 yay -S brave-bin touchegg gnome-shell-extension-pop-shell-bin
 
 chsh -s $(which fish) $USER
+# Don't enable if bluetooth isn't needed
+systemctl enable --now bluetooth.service
 
 # Keep going in fish
 ./bin/install.fish
