b5bca5ffcfa93c766e50c0f0304e4bfc1bf62913
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-12-09 20:32:22 -0800

    Just toggle wifi off and on on sleep

--- hw_specific/lenovo_z13/sleep_script.sh
+++ hw_specific/lenovo_z13/sleep_script.sh
@@ -5,12 +5,11 @@ PATH=/sbin:/usr/sbin:/bin:/usr/bin
 case "$1" in
 	#code execution BEFORE sleeping/hibernating/suspending
 	pre)
-		xinput set-prop "ELAN06A0:00 04F3:3231 Touchpad" "Device Enabled" 0
+		rfkill block wlan
 	;;
 	#code execution AFTER resuming
 	post)
-		# Toggle touchpad to make two finger right clicks more reliable
-		xinput set-prop "ELAN06A0:00 04F3:3231 Touchpad" "Device Enabled" 1
+		rfkill unblock wlan
 	;;
 esac
 
