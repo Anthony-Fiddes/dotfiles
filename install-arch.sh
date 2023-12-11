df0a5f555d59b3db82cb948307be37d7397b9e8d
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-12-10 20:27:20 -0800

    Add nss-mdns and avahi installation

--- install-arch.sh
+++ install-arch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 yay -S fish kitty neovim fzf ripgrep github-cli go htop \
-  bat xclip flatpak git-delta
+  bat xclip flatpak git-delta nss-mdns avahi
 # AUR
 yay -S brave-bin gnome-shell-extension-pop-shell-bin pyenv-virtualenv
 
@@ -8,6 +8,13 @@ chsh -s $(which fish) $USER
 
 # Don't enable if bluetooth isn't needed
 systemctl enable --now bluetooth.service
+
+# Disable resolved so that it doesn't conflict with avahi
+# TODO: maybe learn the benefits of resolved and if that pans out, configure it
+# not to conflict with avahi. I think it was disabled by default on EndeavourOS
+systemctl disable --now systemd-resolved.service
+# Use avahi to resolve mDNS requests
+systemctl enable --now avahi-daemon.service
 
 # Keep going in fish
 ./bin/install.fish
