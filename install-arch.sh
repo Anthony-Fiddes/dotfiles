0ce09768eb8b6297149358b2d0e2a7deb4e309bd
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-12-10 19:53:33 -0800

    Fix space

--- install-arch.sh
+++ install-arch.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 yay -S fish kitty neovim fzf ripgrep github-cli go htop \
-  bat xclip  flatpak git-delta
+  bat xclip flatpak git-delta
 # AUR
 yay -S brave-bin gnome-shell-extension-pop-shell-bin pyenv-virtualenv
 
 chsh -s $(which fish) $USER
+
 # Don't enable if bluetooth isn't needed
 systemctl enable --now bluetooth.service
 
