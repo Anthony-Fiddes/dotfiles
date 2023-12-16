e2780dc0ff37c36bd9ce0f998cc8f40a2761147e
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-12-16 14:22:12 -0800

    Add some print statements to the sleep script

--- hw_specific/lenovo_z13/sleep_script.sh
+++ hw_specific/lenovo_z13/sleep_script.sh
@@ -5,10 +5,12 @@ PATH=/sbin:/usr/sbin:/bin:/usr/bin
 case "$1" in
 	#code execution BEFORE sleeping/hibernating/suspending
 	pre)
+		echo "Turning off wlan"
 		rfkill block wlan
 	;;
 	#code execution AFTER resuming
 	post)
+		echo "Turning wlan back on"
 		rfkill unblock wlan
 	;;
 esac
