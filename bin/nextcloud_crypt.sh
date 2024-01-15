8f20d7ffaae7b5cce4fe06d5eb955fddba225f95
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-01-15 09:46:23 -0800

    Simplify nextcloud_crypt install

--- bin/nextcloud_crypt.sh
+++ bin/nextcloud_crypt.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 DRIVE=/media/nextcloud_crypt
-sudo mkdir -p $DRIVE
-sudo chown $(whoami) $DRIVE
-fusermount -u $DRIVE 
-rclone mount nextcloud_crypt: $DRIVE --vfs-cache-mode full 
+fusermount -u $DRIVE
+rclone mount nextcloud_crypt: $DRIVE --vfs-cache-mode full
--- os_specific/systemd/install_nextcloud_crypt.sh
+++ os_specific/systemd/install_nextcloud_crypt.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 target=$XDG_CONFIG_HOME/systemd/user
-sudo mkdir -p /media/nextcloud_crypt
+drive=/media/nextcloud_crypt
+fusermount -u $drive
+sudo mkdir -p $drive
+sudo chown $(whoami) $drive
 mkdir -p $target
 cp ./user/nextcloud_crypt.service $target
 # the config is available in Bitwarden
