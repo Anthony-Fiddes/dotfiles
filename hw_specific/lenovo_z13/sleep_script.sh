50b09e00497138c5ce68356cf7172e61924c2073
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-01-16 17:49:49 -0800

    toggle wlan twice per null.rip article

--- hw_specific/lenovo_z13/sleep_script.sh
+++ hw_specific/lenovo_z13/sleep_script.sh
@@ -5,13 +5,13 @@ PATH=/sbin:/usr/sbin:/bin:/usr/bin
 case "$1" in
 	#code execution BEFORE sleeping/hibernating/suspending
 	pre)
-		echo "Turning off wlan"
-		rfkill block wlan
 	;;
 	#code execution AFTER resuming
 	post)
-		echo "Turning wlan back on"
-		rfkill unblock wlan
+		# per https://null.rip/2022/09/linux-on-the-thinkpad-z13-all-amd-almost-there/
+		echo "Toggling wlan twice"
+		rfkill toggle wlan
+		rfkill toggle wlan
 	;;
 esac
 
