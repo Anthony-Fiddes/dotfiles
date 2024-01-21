38fd47a4279d76cb8c238d1e6aadb620d9c39420
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-01-21 15:59:45 -0800

    Use \? instead of * in sed command to disable bluetooth by default

--- hw_specific/lenovo_z13/install-arch.sh
+++ hw_specific/lenovo_z13/install-arch.sh
@@ -1,2 +1,2 @@
 # Don't turn on bluetooth by default, saves battery
-sudo sed -i "s/#*AutoEnable=true/AutoEnable=false/" /etc/bluetooth/main.conf
+sudo sed -i "s/#\?AutoEnable=true/AutoEnable=false/" /etc/bluetooth/main.conf
