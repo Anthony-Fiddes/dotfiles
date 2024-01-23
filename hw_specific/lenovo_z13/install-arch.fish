ff653f3b5826b98ac704db4ca35ef4dc8b2d891a
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-01-22 19:41:29 -0800

    Install tlp, remove install-arch.fish

--- hw_specific/lenovo_z13/install-arch.fish
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/fish
-# Install sleep script
-sudo cp ./sleep_script.sh /usr/lib/systemd/system-sleep/
--- hw_specific/lenovo_z13/install-arch.sh
+++ hw_specific/lenovo_z13/install-arch.sh
@@ -1,2 +1,14 @@
+# Install sleep script
+sudo cp ./sleep_script.sh /usr/lib/systemd/system-sleep/
+
 # Don't turn on bluetooth by default, saves battery
-sudo sed -i "s/#\?AutoEnable=true/AutoEnable=false/" /etc/bluetooth/main.conf
+sudo sed -i "s/#\?AutoEnable=true/AutoEnable=false/1" /etc/bluetooth/main.conf
+
+# Useful guide: https://linrunner.de/tlp/faq/ppd.html
+yay -R power-profiles-daemon
+yay -S tlp
+sudo sed -i "s/#\?PLATFORM_PROFILE_ON_AC=.*/PLATFORM_PROFILE_ON_AC=performance/1" /etc/tlp.conf
+sudo sed -i "s/#\?PLATFORM_PROFILE_ON_BAT=.*/PLATFORM_PROFILE_ON_BAT=low-power/1" /etc/tlp.conf
+systemctl enable tlp.service
+systemctl mask systemd-rfkill.service systemd-rfkill.socket
+echo "Run sudo tlp start or restart"
