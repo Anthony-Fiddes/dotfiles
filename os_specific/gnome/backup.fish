b55d34b8560c9879813e3001993f27d3169928a3
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-03-04 10:39:20 -0800

    Delete conflicting keybindings

--- os_specific/gnome/backup.fish
+++ os_specific/gnome/backup.fish
@@ -2,3 +2,4 @@
 cp $XDG_CONFIG_HOME/touchegg/touchegg.conf ./touchegg.conf
 dconf dump /org/gnome/desktop/wm/keybindings/ >keybindings.dconf
 dconf dump /org/gnome/shell/extensions/pop-shell/ >pop-shell.dconf
+dconf dump /org/gnome/shell/keybindings/ >gnome-shell-keybindings.dconf
--- os_specific/gnome/install.fish
+++ os_specific/gnome/install.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/fish
+# TODO: test for the folder's existence
 cp ./touchegg.conf $XDG_CONFIG_HOME/touchegg/touchegg.conf
 dconf load /org/gnome/desktop/wm/keybindings/ <keybindings.dconf
 dconf load /org/gnome/shell/extensions/pop-shell/ <pop-shell.dconf
-
+dconf load /org/gnome/shell/keybindings/ <gnome-shell-keybindings.dconf
