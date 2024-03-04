dfb00b79ec49ea4697b714320dbadbde9fc37e9f
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-03-04 12:32:03 -0800

    Test for touchegg and correctly get mutter config

--- os_specific/gnome/backup.fish
+++ os_specific/gnome/backup.fish
@@ -1,5 +1,9 @@
 #!/usr/bin/fish
-cp $XDG_CONFIG_HOME/touchegg/touchegg.conf ./touchegg.conf
+set touchegg_dir $XDG_CONFIG_HOME/touchegg
+if test -e $touchegg_dir
+    cp $touchegg_dir/touchegg.conf ./touchegg.conf
+end
 dconf dump /org/gnome/desktop/wm/keybindings/ >keybindings.dconf
 dconf dump /org/gnome/shell/extensions/pop-shell/ >pop-shell.dconf
 dconf dump /org/gnome/shell/keybindings/ >gnome-shell-keybindings.dconf
+dconf dump /org/gnome/mutter/ >mutter.dconf
--- os_specific/gnome/install.fish
+++ os_specific/gnome/install.fish
@@ -1,6 +1,9 @@
 #!/usr/bin/fish
-# TODO: test for the folder's existence
-cp ./touchegg.conf $XDG_CONFIG_HOME/touchegg/touchegg.conf
+set touchegg_dir $XDG_CONFIG_HOME/touchegg
+if test -e $touchegg_dir
+    cp ./touchegg.conf $touchegg_dir/touchegg.conf
+end
 dconf load /org/gnome/desktop/wm/keybindings/ <keybindings.dconf
 dconf load /org/gnome/shell/extensions/pop-shell/ <pop-shell.dconf
 dconf load /org/gnome/shell/keybindings/ <gnome-shell-keybindings.dconf
+dconf load /org/gnome/mutter/ <mutter.dconf
