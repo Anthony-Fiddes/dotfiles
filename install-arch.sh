c737f9bc06a49ef1b0faf096f81783b15d875810
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-03-09 21:46:07 -0800

    Add gnome-contacts

--- install-arch.sh
+++ install-arch.sh
@@ -7,7 +7,7 @@ yay -S fish neovim fzf ripgrep github-cli go htop \
 yay -S brave-bin pyenv-virtualenv timeshift-autosnap librewolf-bin
 # Gnome stuff
 yay -S gnome-shell-extension-appindicator dconf-editor gnome-calendar \
-  gnome-software gnome-firmware
+  gnome-contacts gnome-software gnome-firmware
 echo "appindicator was installed. Restart gnome and enable it in the extensions app."
 
 ## baobab is the gnome disk usage analyzer
