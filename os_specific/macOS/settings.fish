ef62df54ef5c923743e5c4585e7aa9fdb38a2ba6
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-05-22 13:24:54 -0700

    Update settings.fish to use a text file to determine what to backup

--- os_specific/macOS/settings.fish
+++ os_specific/macOS/settings.fish
@@ -1,14 +1,19 @@
 #!/usr/bin/env fish
 
-set plist com.apple.symbolichotkeys
-set backupFile ./my-macos-keyboard-shortcuts.xml
-
 # read more about this at https://flaky.build/backup-your-macos-keyboard-shortcuts-from-command-line
 switch $argv[1]
     case backup
-        defaults export $plist - >$backupFile
+        for line in $(cat ./settings-to-backup.txt)
+            set pair (string split " -> " $line)
+            defaults export $pair[1] - >$pair[2]
+            echo "$pair[1] -> $pair[2]"
+        end
     case load
-        defaults import $plist $backupFile
+        for line in $(cat ./settings-to-backup.txt)
+            set pair (string split " -> " $line)
+            defaults import $pair[1] $pair[2]
+            echo "$pair[2] -> $pair[1]"
+        end
         and /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
     case '*'
         echo "Usage: $(status filename) <backup|load>"
