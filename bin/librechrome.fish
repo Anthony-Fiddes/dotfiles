07d0ba4dd8c20031b75ae7a23743b4c77a2ba710
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-06-02 21:14:01 -0700

    Hopefully make librechrome work on linux too

--- bin/librechrome.fish
+++ bin/librechrome.fish
@@ -5,10 +5,14 @@ if test $(count $argv) -ne 1
     echo "file: A file to be copied into the current librewolf profile directory."
     exit 1
 end
+
 set target $argv[1]
-set profiles_dir (fd -H --full-path librewolf/Profiles\$ ~ | rg -v '(C|c)ache')
-set separator "Default=Profiles/"
-set profile_line (rg $separator $profiles_dir/../profiles.ini)
+set profiles_dir (fd -H --full-path 'librewolf(/Profiles)?$' ~ | rg -v '(C|c)ache')
+set separator "Default="
+set profiles_ini (fd profiles.ini $profiles_dir)
+# search profiles.ini from the bottom up since there's usually a Default=1
+# profile at the top of the file
+set profile_line (rg $separator $profiles_ini | tac)[1]
 set profile_dir $profiles_dir/(echo $profile_line | string replace $separator "")
 mkdir $profile_dir/chrome
 echo "Copying $target to $profile_dir/chrome"
