2e1fa477563d9868b7c4b3b72cfc4f2983ed9b79
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-06-03 19:43:33 -0700

    Rename profiles_dir to librewolf_dir

--- bin/librechrome.fish
+++ bin/librechrome.fish
@@ -7,13 +7,13 @@ if test $(count $argv) -ne 1
 end
 
 set target $argv[1]
-set profiles_dir (fd -H --full-path 'librewolf$' ~ | rg -v '(C|c)ache' | rg -v 'dotfiles')[1]
+set librewolf_dir (fd -H --full-path 'librewolf$' ~ | rg -v '(C|c)ache' | rg -v 'dotfiles')[1]
 set separator "Default="
-set profiles_ini (fd profiles.ini $profiles_dir)
+set profiles_ini (fd profiles.ini $librewolf_dir)
 # search profiles.ini from the bottom up since there's usually a Default=1
 # profile at the top of the file
 set profile_line (rg $separator $profiles_ini | tac)[1]
-set profile_dir $profiles_dir/(echo $profile_line | string replace $separator "")
+set profile_dir $librewolf_dir/(echo $profile_line | string replace $separator "")
 mkdir $profile_dir/chrome
 echo "Copying $target to $profile_dir/chrome"
 cp $target $profile_dir/chrome/
