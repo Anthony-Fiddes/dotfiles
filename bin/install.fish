7fd2a20da8175af8daeb5107ccba9fc009cfeb21
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-08-22 11:37:49 +0200

    Actually set kitty.desktop in xdg-terminals.list

--- bin/install.fish
+++ bin/install.fish
@@ -35,6 +35,7 @@ if not type -q kitty
     cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
     sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
     sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+    echo 'kitty.desktop' >~/.config/xdg-terminals.list
 end
 rm -r $XDG_CONFIG_HOME/kitty
 ln -s $(pwd)/kitty $XDG_CONFIG_HOME/kitty
