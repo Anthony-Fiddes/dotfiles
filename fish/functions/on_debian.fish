24c5be3072ceb1d0bcf3a6e8b702f5328a1a6ab1
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2024-09-22 11:21:52 +0200

    Make on_debian silent by testing for the existence of /etc/os-release before "cat"ing it

--- fish/functions/on_debian.fish
+++ fish/functions/on_debian.fish
@@ -1,4 +1,7 @@
 # on_debian checks if the current shell is using a Debian based distro.
 function on_debian
-    on_ubuntu; or cat /etc/os-release | grep -q -i debian
+    if test -e /etc/os-release
+        return (on_ubuntu; or cat /etc/os-release | grep -q -i debian)
+    end
+    on_ubuntu
 end
