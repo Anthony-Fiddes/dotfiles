7fd589097b60ad04a77f86a723fba013c98eba8a
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2025-02-01 12:47:58 +0100

    feat: make nextcloud_crypt_mount a bit more cross-platform

--- bin/nextcloud_crypt.sh
+++ bin/nextcloud_crypt.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 DRIVE=~/nextcloud_crypt_mount
 mkdir -p $DRIVE
-fusermount -u $DRIVE
-rclone mount nextcloud_crypt: $DRIVE --vfs-cache-mode full
+if [ -x "$(command -v fusermount)" ]; then
+   # this way has always worked on Linux, so I left it the way it was
+   fusermount -u $DRIVE
+   rclone mount nextcloud_crypt: $DRIVE --vfs-cache-mode full
+elif [ -x "$(command -v umount)" ]; then
+   # presumed macos
+   umount $DRIVE
+   rclone nfsmount nextcloud_crypt: $DRIVE --vfs-cache-mode=full
+else
+   echo "No supported commands found. Unable to mount drive."
+fi
