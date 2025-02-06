0c2cc2591be81260339a30209d717037c39eaa4e
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2025-02-06 14:38:50 +0100

    docs: document how rm needs to be called in install-config.fish

--- install-config.fish
+++ install-config.fish
@@ -2,6 +2,10 @@
 # the idea behind this file is that it should be idempotent. Run it as many
 # times as you like, and get the same result.
 
+# it's very important that there's not a trailing slash on these rm -r calls.
+# This way, if it's a symlink, only the link is deleted. If it's a folder, the
+# folder gets deleted.
+
 rm -r $XDG_CONFIG_HOME/nvim
 ln -s $(pwd)/config/nvim $XDG_CONFIG_HOME/nvim
 
