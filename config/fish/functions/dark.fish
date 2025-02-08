5e246998f1fb4642feee6d6a1c56af62364d85f8
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2025-02-08 23:09:11 +0100

    feat(fish): create functions for switching to light and dark mode

---
+++ config/fish/functions/dark.fish
@@ -0,0 +1,14 @@
+function dark
+    # made with reference to this great article:
+    # https://evantravers.com/articles/2022/02/08/light-dark-toggle-for-neovim-fish-and-kitty/
+    set -Ux THEME dark
+    kitty @ set-colors -a $XDG_CONFIG_HOME/kitty/dark-theme.conf
+
+    set -Ux FZF_DEFAULT_OPTS "\
+        --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
+        --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
+        --color=marker:#b7bdf8,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
+        --color=selected-bg:#494d64 \
+        --multi"
+    # TODO: configure bat
+end
---
+++ config/fish/functions/light.fish
@@ -0,0 +1,13 @@
+function light
+    # made with reference to this great article:
+    # https://evantravers.com/articles/2022/02/08/light-dark-toggle-for-neovim-fish-and-kitty/
+    set -Ux THEME light
+    kitty @ set-colors -a $XDG_CONFIG_HOME/kitty/light-theme.conf
+
+    set -Ux FZF_DEFAULT_OPTS "\
+        --color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+        --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+        --color=marker:#7287fd,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
+        --color=selected-bg:#bcc0cc \
+        --multi"
+end
