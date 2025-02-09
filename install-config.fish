6e8b2900173c67cd33def3bf8d44a1a08b5d20c8
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2025-02-09 10:06:48 +0100

    feat(fish): simplify install-config to work for any dir in config/

--- install-config.fish
+++ install-config.fish
@@ -6,14 +6,13 @@
 # This way, if it's a symlink, only the link is deleted. If it's a folder, the
 # folder gets deleted.
 
-rm -r $XDG_CONFIG_HOME/nvim
-ln -s $(pwd)/config/nvim $XDG_CONFIG_HOME/nvim
-
-rm -r $XDG_CONFIG_HOME/fish
-ln -s $(pwd)/config/fish $XDG_CONFIG_HOME/fish
-
-rm -r $XDG_CONFIG_HOME/kitty
-ln -s $(pwd)/config/kitty $XDG_CONFIG_HOME/kitty
+for dir in config/*/
+    # basename removes the trailing slash
+    set base (basename $dir)
+    set target_dir $XDG_CONFIG_HOME/$base
+    rm -r $target_dir
+    ln -s $(pwd)/$dir $target_dir
+end
 
 mkdir -p $XDG_CONFIG_HOME/yamllint/
 ln -sf $(pwd)/config/yamllint.yaml $XDG_CONFIG_HOME/yamllint/config
