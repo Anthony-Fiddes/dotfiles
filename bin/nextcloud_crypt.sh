f46b68bfb2eb547ca0cae3aff5ecbf5ea1c9827e
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2026-03-17 20:33:21 +0100

    fix(nextcloud): cache files for a year locally, max 50GB

--- bin/nextcloud_crypt.sh
+++ bin/nextcloud_crypt.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 DRIVE=~/nextcloud_crypt_mount
+CACHE_FLAGS=(--vfs-cache-mode full --vfs-cache-max-age 52w --vfs-cache-max-size 50G)
 mkdir -p $DRIVE
 if [ -x "$(command -v fusermount)" ]; then
    # this way has always worked on Linux, so I left it the way it was
    fusermount -u $DRIVE
-   rclone mount nextcloud_crypt: $DRIVE --vfs-cache-mode full
+   rclone mount nextcloud_crypt: $DRIVE "${CACHE_FLAGS[@]}"
 elif [ -x "$(command -v umount)" ]; then
    # presumed macos
    umount $DRIVE
-   rclone nfsmount nextcloud_crypt: $DRIVE --vfs-cache-mode full
+   rclone nfsmount nextcloud_crypt: $DRIVE "${CACHE_FLAGS[@]}"
 else
    echo "No supported commands found. Unable to mount drive."
 fi
